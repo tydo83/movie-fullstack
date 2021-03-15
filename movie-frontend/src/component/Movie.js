87c71aa397dd6e5701d212d5f121b4bbe67604bf
Timothy Do <tydo83@gmail.com>
2021-03-15 03:20:38 -0400

    ver 3

--- movie-frontend/src/component/Movie.js
+++ movie-frontend/src/component/Movie.js
@@ -62,7 +62,7 @@ export class Movie extends Component {
         }
     };
 
-    updateOnChange = (event) => {
+    updatedOnChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value,
         });
